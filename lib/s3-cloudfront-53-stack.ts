a0042d637d7461be4d3ac064316045a8cbdfefd2
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-06-07 15:38:18 +0545

    www and non www/

--- lib/s3-cloudfront-53-stack.ts
+++ lib/s3-cloudfront-53-stack.ts
@@ -6,7 +6,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as certManager from 'aws-cdk-lib/aws-certificatemanager';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { CloudFrontTarget, Route53RecordTarget } from 'aws-cdk-lib/aws-route53-targets';
 
 export class S3Cloudfront53Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,22 +24,41 @@ export class S3Cloudfront53Stack extends cdk.Stack {
     const hostedZone = route53.HostedZone.fromLookup(this, 'website-hostedzone', { domainName: DOMAIN_NAME });
 
     const certificate = new certManager.Certificate(this, 'website-certificate', {
-      domainName: DOMAIN_NAME,
+      domainName: `*.${DOMAIN_NAME}`,
+      subjectAlternativeNames: [DOMAIN_NAME],
       validation: certManager.CertificateValidation.fromDns(),
     });
 
     const siteDistribution = new cloudfront.Distribution(this, "SiteDistribution", {
       defaultRootObject: 'index.html',
-      domainNames: [DOMAIN_NAME],
+      domainNames: [`www.${DOMAIN_NAME}`, DOMAIN_NAME],
       certificate: certificate,
       defaultBehavior: {
-        origin: new S3Origin(bucket)
+        origin: new S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.ALLOW_ALL
       }
     });
 
-    new route53.ARecord(this, 'web-dns-A-record', {
+    // to redirect www to non www, we can make bucket with www.domain.com and use domain.com as redirection
+
+    // or make two a records pointing to same coludfront. bad for seo
+
+    new route53.ARecord(this, 'web-dns-www-A-record', {
+      recordName: 'www',
       zone: hostedZone,
       target: route53.RecordTarget.fromAlias(new CloudFrontTarget(siteDistribution))
+    })
+    new route53.ARecord(this, 'web-dns-Bare-A-record', {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new CloudFrontTarget(siteDistribution))
+    })
+
+    // s3.amritpaudel.me
+    // only works if bucket name is s3.amritpaudel.me
+    new route53.CnameRecord(this, 'web-dns-cname-record', {
+      zone: hostedZone,
+      domainName: bucket.bucketWebsiteDomainName,
+      recordName: 's3'
     })
 
 
@@ -53,6 +72,11 @@ export class S3Cloudfront53Stack extends cdk.Stack {
       value: DOMAIN_NAME,
       description: 'custom url',
       exportName: 'websiteUrl',
+    });
+
+    new cdk.CfnOutput(this, 'bucket-website-url', {
+      value: bucket.bucketDomainName,
+      exportName: 'bucketwebsiteUrl',
     });
 
   }
